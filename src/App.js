a8a6b8b189a441e5959a9939191e00607e68e1c9
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-07-07 13:43:50 +0300

    added navigation components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import Directory from "./components/directory/directory.component";
-import "./app.scss";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+import Home from "./routes/home/home.component.jsx";
+import Shop from "./routes/shop/shop.component.jsx";
+import Contact from "./routes/contact/contact.component.jsx";
+import SignIn from "./routes/signIn/signIn.component.jsx";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "women",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="signIn" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
