50beaa8f61f46e76d1bac4359a1e74da8eb9cc77
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-07-16 16:32:45 +0300

    added cart functionality, adding items to it, counting total amount of items

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 
@@ -11,7 +13,11 @@ root.render(
   //<React.StrictMode>
   <Router>
     <UserProvider>
-      <App />
+      <ProductsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ProductsProvider>
     </UserProvider>
   </Router>
   //</React.StrictMode>
