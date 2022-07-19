2e4be85eaef8f208c8ff9408be08acc8de10bbd7
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-07-19 15:28:33 +0300

    full version of shopping page, Links modified

--- src/routes/category-page/category-page.component.jsx
+++ src/routes/category-page/category-page.component.jsx
@@ -5,22 +5,23 @@ import ProductCard from "../../components/product-card/product-card.component";
 import "./category-page.styles.scss";
 
 const CategoryPage = () => {
-  // console.log(useParams());
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]); // gets products of chosen category
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
-  // console.log(categoriesMap);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
-    </div>
+    <>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
+      </div>
+    </>
   );
 };
 
