863f6f9fd938741faef7b8d11ca80bdf0d539d2d
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-08-23 11:12:42 +0300

    full mobile-friednly site

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,18 +1,26 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
+import {
+  selectCartItems,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isCartOpen = useSelector(selectIsCartOpen);
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   const cartItems = useSelector(selectCartItems);
 
   const goToCheckoutHandler = () => {
+    toggleIsCartOpen();
     navigate("/checkout");
   };
 
